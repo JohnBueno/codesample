64985fd280fb85fb8c6d91838baba69a66b51765
John McCartney <jmm6026@bueno-3.local>
2015-04-13 12:56:01 -0400

    Created venue model and noted clientId and clientSecret for foursquare

---
+++ UrbnCodeSample/Venues.h
@@ -0,0 +1,13 @@
+//
+//  Venues.h
+//  UrbnCodeSample
+//
+//  Created by John McCartney on 4/13/15.
+//  Copyright (c) 2015 John McCartney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Venues : NSObject
+
+@end
---
+++ UrbnCodeSample/Venues.m
@@ -0,0 +1,19 @@
+//
+//  Venues.m
+//  UrbnCodeSample
+//
+//  Created by John McCartney on 4/13/15.
+//  Copyright (c) 2015 John McCartney. All rights reserved.
+//
+
+#import "Venues.h"
+
+// ClientId
+// YDU2NKEWT5N2VQPROGBXBUENW0GR3R4P41KDEZG1XLW5OANJ
+
+// ClientSecret
+// CTRLBHZGCDW5WYQSSOYFBZ0VGTUNMXJI1DGPHDRWV3DNCUU0
+
+@implementation Venues
+
+@end
