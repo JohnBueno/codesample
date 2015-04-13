ec46a731cdebcb891cd91ccd501c75b301806d8f
John McCartney <jmm6026@bueno-3.local>
2015-04-13 15:31:22 -0400

    Added core data singleton

---
+++ UrbnCodeSample/CoreDataStack.h
@@ -0,0 +1,21 @@
+//
+//  CoreDataStack.h
+//  UrbnCodeSample
+//
+//  Created by John McCartney on 4/13/15.
+//  Copyright (c) 2015 John McCartney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface CoreDataStack : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
++(instancetype)defaultStack;
+@end
